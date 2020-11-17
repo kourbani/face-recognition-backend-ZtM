eceabd0805372598719bc40d82a2acc4128a2bcc
kourbani <kanidoux@gmail.com>
2020-11-17 12:17:10 +0200

    Testing Signin route

--- server.js
+++ server.js
@@ -32,6 +32,14 @@ app.get('/', (req,res) => {
   res.send('app is working');
 });
 
+app.post('/signin', (req,res) => {
+  if(req.body.email === database.users[0].email 
+    && req.body.password === database.users[0].password){
+      res.json('success');
+    } else {
+      res.status(400).json('error logging in');
+    }
+});
 
 app.listen(SERVER_PORT, ()=>{
   console.log('app running on port ' + SERVER_PORT)
