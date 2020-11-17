cffd84d40c57c5f8f905565e7eebe75ceba82f52
kourbani <kanidoux@gmail.com>
2020-11-17 12:52:06 +0200

    Testing profile route

--- server.js
+++ server.js
@@ -54,6 +54,20 @@ app.post('/register' , (req,res) => {
  res.json(database.users[database.users.length-1]);
 });
 
+app.get('/profile/:id', (req, res) => {
+  const { id } = req.params;
+  let found = false;
+  database.users.forEach((user) => {
+    if (user.id === id) {
+      found = true;
+      return res.json(user);
+    }
+  });
+  if (!found) {
+    res.status(400).json('not found');
+  }
+});
+
 app.listen(SERVER_PORT, ()=>{
   console.log('app running on port ' + SERVER_PORT)
 });
