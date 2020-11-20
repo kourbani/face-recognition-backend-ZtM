d4c13825b88b53cc49184db3059e1f0c395e1339
kourbani <kanidoux@gmail.com>
2020-11-20 17:38:42 +0200

    Create separate files for routes

---
+++ controllers/entries.js
@@ -0,0 +1,15 @@
+const updateEntries = (req,res,db) => {
+  const { id } = req.body;
+  db('users')
+    .where({ id: id })
+    .increment('entries', 1)
+    .returning('entries')
+    .then((entries) => {
+      if (entries[0].length) {
+        res.json(entries);
+      }
+    })
+    .catch((err) => res.status(400).json('unable to get entries'));
+}
+
+export {updateEntries};
--- server.js
+++ server.js
@@ -2,7 +2,13 @@ import express from 'express';
 import bcrypt from 'bcryptjs';
 import cors from 'cors';
 import knex from 'knex';
+// Controllers
+import {handleRegister} from './controllers/register.js';
+import {handleSignIn} from './controllers/signin.js';
+import {updateEntries} from './controllers/entries.js';
+import {viewProfile} from './controllers/profile.js';
 
+// Connect Database
 const db=knex({
   client: 'pg',
   connection: {
@@ -19,107 +25,22 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// DATABASE
-const database = {
-  users: [
-    {
-      id: '123',
-      name: 'John',
-      email: 'john@gmail.com',
-      password: 'cookies',
-      entries: 0,
-      joined: new Date(),
-    },
-    {
-      id: '124',
-      name: 'Sally',
-      email: 'sally@gmail.com',
-      password: 'bananas',
-      entries: 0,
-      joined: new Date(),
-    }
-  ]
-};
 
 // ROUTES
-app.get('/', (req,res) => {
-  res.send(database.users);
-});
-
 app.post('/signin', (req,res) => {
-  db.select('email' , 'hash').from('login')
-    .where('email','=', req.body.email)
-    .then(data =>{
-      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
-      if(isValid) {
-        return db.select('*').from('users')
-          .where('email','=',req.body.email)
-          .then(user => {
-            res.json(user[0]);
-          })
-          .catch(err => res.status(400).json('unable to get user'));
-      }else {
-        res.status(400).json('Wrong credentials');
-      }
-    })
-    .catch(err => res.status(400).json('Wrong credentials'));
+  handleSignIn(req,res,db,bcrypt);
 });
 
 app.post('/register' , (req,res) => {
- const {name,email,password} = req.body;
- const hash = bcrypt.hashSync(password,10);
-
- db.transaction(trx => {
-  trx.insert({
-    hash: hash,
-    email: email
-  })
-  .into('login')
-  .returning('email')
-  .then(loginEmail => {
-    return trx('users')
-      .returning('*')
-      .insert({
-        name: name,
-        email: loginEmail[0],
-        joined: new Date()
-      })
-      .then((user) => {
-        res.json(user[0]);
-      });
-  })
-  .then(trx.commit)
-  .catch(trx.rollback)
- })
- .catch(err => res.status(400).json('unable to register'));
+  handleRegister(req,res,db,bcrypt);
 });
 
 app.get('/profile/:id', (req, res) => {
-  const { id } = req.params;
-  db.select('*').from('users').where({
-    id: id
-  })
-  .then(user => {
-    if(user.length) {
-      res.json(user[0]);
-    } else {
-      res.json('Not found');
-    }
-  })
-  .catch(err=>res.status(400).json('error getting user'));
+  viewProfile(req,res,db);
 });
 
 app.put('/image', (req, res) => {
-  const { id } = req.body;
-  db('users').where({ id: id })
-    .increment('entries', 1)
-    .returning('entries')
-    .then(entries => {
-      if(entries[0].length){
-        res.json(`Updated Entries to ${entries}`);
-      }     
-    })
-    .catch((err) => res.status(400).json('unable to get entries'));
+  updateEntries(req,res,db);
 });
 
 app.listen(SERVER_PORT, ()=>{
