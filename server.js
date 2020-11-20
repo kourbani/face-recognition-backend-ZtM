c4c5d99006f022948da416a3abcb004208fdb2a3
kourbani <kanidoux@gmail.com>
2020-11-20 14:19:05 +0200

    Register Route - Hash password and make transaction btw db tables: users & login

--- server.js
+++ server.js
@@ -57,14 +57,29 @@ app.post('/signin', (req,res) => {
 
 app.post('/register' , (req,res) => {
  const {name,email,password} = req.body;
- db('users')
- .returning('*')
- .insert({
-   name: name,
-   email: email,
-   joined: new Date()
- }).then(user => {
-    res.json(user[0]);
+ const hash = bcrypt.hashSync(password,10);
+
+ db.transaction(trx => {
+  trx.insert({
+    hash: hash,
+    email: email
+  })
+  .into('login')
+  .returning('email')
+  .then(loginEmail => {
+    return trx('users')
+      .returning('*')
+      .insert({
+        name: name,
+        email: loginEmail[0],
+        joined: new Date()
+      })
+      .then((user) => {
+        res.json(user[0]);
+      });
+  })
+  .then(trx.commit)
+  .catch(trx.rollback)
  })
  .catch(err => res.status(400).json('unable to register'));
 });
